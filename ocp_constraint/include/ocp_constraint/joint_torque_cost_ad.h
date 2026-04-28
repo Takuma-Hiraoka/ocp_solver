9b7a9cdeaf69c49ca03f1bc9eac9d41ce1920d75
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2026-04-28 17:08:35 +0900

    add joint_torque_cost

---
+++ ocp_constraint/include/ocp_constraint/joint_torque_cost_ad.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <ocs2_core/cost/StateInputGaussNewtonCostAd.h>
+#include <ocs2_pinocchio_interface/PinocchioInterface.h>
+
+#include "ocp_solver/state_converter.h"
+
+namespace ocp_constraint {
+
+  class JointTorqueCostCppAd final : public ocs2::StateInputCostGaussNewtonAd {
+  public:
+    JointTorqueCostCppAd(const ocs2::vector_t& weights,
+                         const ocs2::PinocchioInterface& pinocchioInterface,
+                         const ocp_solver::StateConverter<ocs2::ad_scalar_t>& stateConverter,
+                         std::string costName,
+                         std::string modelFolder = "build/cppad_autocode_gen",
+                         bool recompileLibrariesCppAd = false);
+
+    ~JointTorqueCostCppAd() override = default;
+    JointTorqueCostCppAd* clone() const override { return new JointTorqueCostCppAd(*this); }
+
+    ocs2::vector_t getParameters(ocs2::scalar_t time,
+                                 const ocs2::TargetTrajectories& targetTrajectories,
+                                 const ocs2::PreComputation& preComputation) const override {
+      return sqrtWeights_;
+    }
+
+  private:
+    JointTorqueCostCppAd(const JointTorqueCostCppAd& other);
+
+    ocs2::ad_vector_t costVectorFunction(ocs2::ad_scalar_t time,
+                                         const ocs2::ad_vector_t& state,
+                                         const ocs2::ad_vector_t& input,
+                                         const ocs2::ad_vector_t& parameters) const override;
+
+    ocs2::vector_t sqrtWeights_;
+
+    mutable ocs2::PinocchioInterfaceCppAd pinocchioInterfaceCppAd_;
+    ocp_solver::StateConverter<ocs2::ad_scalar_t>* stateConverterPtr_;
+  };
+
+}
---
+++ ocp_constraint/src/joint_torque_cost_ad.cpp
@@ -0,0 +1,43 @@
+#include <pinocchio/fwd.hpp>
+
+#include <ocs2_pinocchio_interface/PinocchioInterface.h>
+
+#include "ocp_constraint/joint_torque_cost_ad.h"
+#include <ocp_solver/dynamics_helper_functions_ad.h>
+
+#include <ocs2_robotic_tools/common/RotationTransforms.h>
+
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/kinematics.hpp>
+#include <pinocchio/multibody/data.hpp>
+#include <pinocchio/multibody/model.hpp>
+
+namespace ocp_constraint {
+  JointTorqueCostCppAd::JointTorqueCostCppAd(const ocs2::vector_t& weights,
+                                             const ocs2::PinocchioInterface& pinocchioInterface,
+                                             const ocp_solver::StateConverter<ocs2::ad_scalar_t>& stateConverter,
+                                             std::string costName,
+                                             std::string modelFolder,
+                                             bool recompileLibrariesCppAd)
+    : StateInputCostGaussNewtonAd(),
+      sqrtWeights_(weights.cwiseSqrt()),
+      pinocchioInterfaceCppAd_(pinocchioInterface.toCppAd()),
+      stateConverterPtr_(stateConverter.clone()) {
+    assert(weights.size() == stateConverter.getJointDim());
+    initialize(stateConverter.getStateDim(), stateConverter.getInputDim(), stateConverter.getJointDim(), costName,
+               modelFolder, recompileLibrariesCppAd);
+  }
+
+  JointTorqueCostCppAd::JointTorqueCostCppAd(const JointTorqueCostCppAd& other)
+    : StateInputCostGaussNewtonAd(other),
+      sqrtWeights_(other.sqrtWeights_),
+      pinocchioInterfaceCppAd_(other.pinocchioInterfaceCppAd_),
+      stateConverterPtr_(other.stateConverterPtr_->clone()) {}
+
+  ocs2::ad_vector_t JointTorqueCostCppAd::costVectorFunction(ocs2::ad_scalar_t time,
+                                                             const ocs2::ad_vector_t& state,
+                                                             const ocs2::ad_vector_t& input,
+                                                             const ocs2::ad_vector_t& parameters) const {
+    return ocp_solver::computeJointTorques<ocs2::ad_scalar_t>(state, input, pinocchioInterfaceCppAd_, *stateConverterPtr_).cwiseProduct(parameters);
+  }
+}
