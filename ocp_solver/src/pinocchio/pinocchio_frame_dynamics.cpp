3e2d9a0d098efdec3d9abd79ec9f1c2539a41e45
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2026-05-22 16:00:01 +0900

    fix

--- ocp_solver/src/pinocchio/pinocchio_frame_dynamics.cpp
+++ ocp_solver/src/pinocchio/pinocchio_frame_dynamics.cpp
@@ -396,7 +396,7 @@ namespace ocp_solver {
       for (int i=0; i<stateConverter_->contactCandidateIds.size(); i++) {
         ocs2::matrix_t J = ocs2::matrix_t::Zero(6, stateConverter_->getTangentDim());
         pinocchio::getFrameJacobian(model, data, stateConverter_->contactCandidateIds[i], rf, J);
-        acceleration.dfdu.block(0,i*6, 6, 6) = data.M.topLeftCorner(6,6).inverse() * J.block(0,0,6,6).transpose() + a_partial_da.leftCols(stateConverter_->getBaseVDim()) * data.M.topLeftCorner(6,6).inverse() * J.block(0,0,6,6).transpose();
+        acceleration.dfdu.block(0,i*6, 6, 6) = a_partial_da.leftCols(stateConverter_->getBaseVDim()) * data.M.topLeftCorner(6,6).inverse() * J.block(0,0,6,6).transpose();
       }
     }
 
--- ocp_solver/src/solver/system_dynamics.cpp
+++ ocp_solver/src/solver/system_dynamics.cpp
@@ -1,10 +1,9 @@
 #include "ocp_solver/solver/system_dynamics.h"
 #include "ocp_solver/solver/dynamics_helper_functions.h"
-#include <pinocchio/algorithm/aba.hpp>
-#include <pinocchio/algorithm/kinematics.hpp>
-#include <pinocchio/algorithm/aba-derivatives.hpp>
 #include <pinocchio/algorithm/crba.hpp>
 #include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/jacobian.hpp>
+#include <pinocchio/algorithm/rnea-derivatives.hpp>
 
 namespace ocp_solver {
   SystemDynamics::SystemDynamics(const ocs2::PinocchioInterface& pinocchioInterface,
@@ -24,51 +23,65 @@ namespace ocp_solver {
   ocs2::VectorFunctionLinearApproximation SystemDynamics::linearApproximation(ocs2::scalar_t t, const ocs2::vector_t& x, const ocs2::vector_t& u,
                                                                               const ocs2::PreComputation& preComputation) {
 
-    ocs2::vector_t tau_eff = ocs2::vector_t::Zero(stateConverter_.getTangentDim());
+    const auto& model = pinInterface_.getModel();
+    auto& data = pinInterface_.getData();
 
-    pinocchio::crba(pinInterface_.getModel(), pinInterface_.getData(), x.head(stateConverter_.getGenCoordinatesDim()));
-    pinocchio::forwardKinematics(pinInterface_.getModel(), pinInterface_.getData(), x.head(stateConverter_.getGenCoordinatesDim()), x.tail(stateConverter_.getTangentDim()));
-
-    pinocchio::container::aligned_vector<pinocchio::Force> fextDesired(pinInterface_.getModel().njoints, pinocchio::Force::Zero());
-    auto setExternalForce = [&](const pinocchio::FrameIndex& frameIndex, size_t i) {
-                              const auto jointIndex = pinInterface_.getModel().frames[frameIndex].parentJoint;
-                              const Eigen::Matrix<ocs2::scalar_t, 3, 1> translationJointFrameToContactFrame = pinInterface_.getModel().frames[frameIndex].placement.translation();
-                              const Eigen::Matrix<ocs2::scalar_t, 3, 3> rotationWorldFrameToJointFrame = pinInterface_.getData().oMi[jointIndex].rotation().transpose();
-                              const Eigen::Matrix<ocs2::scalar_t, 3, 1> contactForce = rotationWorldFrameToJointFrame * stateConverter_.getContactWrench(u, i).head(3);
-                              const Eigen::Matrix<ocs2::scalar_t, 3, 1> contactTorque = rotationWorldFrameToJointFrame * stateConverter_.getContactWrench(u, i).tail(3);
-                              fextDesired[jointIndex].linear() = contactForce;
-                              fextDesired[jointIndex].angular() = translationJointFrameToContactFrame.cross(contactForce) + contactTorque;
-                            };
-    for (int i=0; i<stateConverter_.contactCandidateIds.size(); i++) setExternalForce(stateConverter_.contactCandidateIds[i], i);
-
-    pinocchio::aba(pinInterface_.getModel(), pinInterface_.getData(), x.head(stateConverter_.getGenCoordinatesDim()), x.tail(stateConverter_.getTangentDim()), tau_eff, fextDesired);
-    pinocchio::computeABADerivatives(pinInterface_.getModel(), pinInterface_.getData(), x.head(stateConverter_.getGenCoordinatesDim()), x.tail(stateConverter_.getTangentDim()), tau_eff, fextDesired);
-    ocs2::matrix_t M_bj = pinInterface_.getData().M.block(0, 6, 6, stateConverter_.getJointDim());
-
-    pinocchio::computeJointJacobians(pinInterface_.getModel(), pinInterface_.getData(), x.head(stateConverter_.getGenCoordinatesDim()));
+    const size_t tangentDim = stateConverter_.getTangentDim();
+    const size_t jointDim = stateConverter_.getJointDim();
+    const ocs2::vector_t q = stateConverter_.getGeneralizedCoordinates(x);
+    const ocs2::vector_t v = stateConverter_.getGeneralizedVelocities(x, u);
+    const ocs2::vector_t qddJoints = stateConverter_.getJointAccelerations(u);
 
     ocs2::VectorFunctionLinearApproximation approximation;
     approximation.dfdx.setZero(stateConverter_.getStateVariableDim(), stateConverter_.getStateVariableDim());
-    approximation.dfdx.topRightCorner(stateConverter_.getTangentDim(), stateConverter_.getTangentDim()) = ocs2::matrix_t::Identity(stateConverter_.getTangentDim(), stateConverter_.getTangentDim());
-    if (stateConverter_.getBaseVDim() == 6) {
-      approximation.dfdx.block(stateConverter_.getTangentDim(), 0, 6, stateConverter_.getTangentDim()) = pinInterface_.getData().ddq_dq.topRows(6);
-      approximation.dfdx.block(stateConverter_.getTangentDim(), stateConverter_.getTangentDim(), 6, stateConverter_.getTangentDim()) = pinInterface_.getData().ddq_dv.topRows(6);
-    }
-
+    approximation.dfdx.topRightCorner(tangentDim, tangentDim) = ocs2::matrix_t::Identity(tangentDim, tangentDim);
     approximation.dfdu.setZero(stateConverter_.getStateVariableDim(), u.rows());
-    if (stateConverter_.getBaseVDim() == 6) {
-      for (int i=0; i<stateConverter_.contactCandidateIds.size(); i++) {
-        ocs2::matrix_t J = ocs2::matrix_t::Zero(6, stateConverter_.getTangentDim());
-        pinocchio::getFrameJacobian(pinInterface_.getModel(), pinInterface_.getData(), stateConverter_.contactCandidateIds[i], pinocchio::ReferenceFrame::LOCAL_WORLD_ALIGNED, J);
-        approximation.dfdu.block(stateConverter_.getTangentDim(), i * 6, 6, 6) = pinInterface_.getData().M.topLeftCorner(6, 6).inverse() * J.block(0, 0, 6, 6).transpose();
-      }
-    }
+    approximation.dfdu.bottomRightCorner(jointDim, jointDim) = ocs2::matrix_t::Identity(jointDim, jointDim);
+    approximation.f = computeStateDerivative<ocs2::scalar_t>(x, u, pinInterface_, stateConverter_);
 
     if (stateConverter_.getBaseVDim() == 6) {
-      approximation.dfdu.block(stateConverter_.getTangentDim(), stateConverter_.getJointAccelerationsStartindex(), 6, stateConverter_.getJointDim()) = - pinInterface_.getData().M.topLeftCorner(6, 6).inverse() * M_bj;
+      ocs2::vector_t qdd(tangentDim);
+      qdd.head(6) = approximation.f.segment(tangentDim, 6);
+      qdd.tail(jointDim) = qddJoints;
+
+      pinocchio::computeRNEADerivatives(model, data, q, v, qdd);
+      const ocs2::matrix_t dtau_dq = data.dtau_dq;
+      const ocs2::matrix_t dtau_dv = data.dtau_dv;
+      ocs2::matrix_t M = data.M;
+      M.triangularView<Eigen::StrictlyLower>() = M.transpose().triangularView<Eigen::StrictlyLower>();
+
+      ocs2::matrix_t externalForcesDerivative = ocs2::matrix_t::Zero(tangentDim, tangentDim);
+      for (size_t column = 0; column < tangentDim; ++column) {
+        ocs2::vector_t tangentDirection = ocs2::vector_t::Zero(tangentDim);
+        tangentDirection(column) = 1.0;
+        pinocchio::computeJointJacobiansTimeVariation(model, data, q, tangentDirection);
+
+        for (size_t i = 0; i < stateConverter_.contactCandidateIds.size(); ++i) {
+          ocs2::matrix_t dJ = ocs2::matrix_t::Zero(6, tangentDim);
+          pinocchio::getFrameJacobianTimeVariation(model, data, stateConverter_.contactCandidateIds[i],
+                                                   pinocchio::ReferenceFrame::LOCAL_WORLD_ALIGNED, dJ);
+          externalForcesDerivative.col(column).noalias() += dJ.transpose() * stateConverter_.getContactWrench(u, i);
+        }
+      }
+
+      const ocs2::matrix_t M_bb = M.topLeftCorner(6, 6);
+      const auto MbbSolver = M_bb.ldlt();
+      const ocs2::matrix_t baseResidualDerivativeQ = dtau_dq.topRows(6) - externalForcesDerivative.topRows(6);
+
+      approximation.dfdx.block(tangentDim, 0, 6, tangentDim) = -MbbSolver.solve(baseResidualDerivativeQ);
+      approximation.dfdx.block(tangentDim, tangentDim, 6, tangentDim) = -MbbSolver.solve(dtau_dv.topRows(6));
+
+      pinocchio::computeJointJacobians(model, data, q);
+      for (size_t i = 0; i < stateConverter_.contactCandidateIds.size(); ++i) {
+        ocs2::matrix_t J = ocs2::matrix_t::Zero(6, tangentDim);
+        pinocchio::getFrameJacobian(model, data, stateConverter_.contactCandidateIds[i], pinocchio::ReferenceFrame::LOCAL_WORLD_ALIGNED, J);
+        approximation.dfdu.block(tangentDim, i * 6, 6, 6) = MbbSolver.solve(J.block(0, 0, 6, 6).transpose());
+      }
+
+      approximation.dfdu.block(tangentDim, stateConverter_.getJointAccelerationsStartindex(), 6, jointDim) =
+        -MbbSolver.solve(M.block(0, 6, 6, jointDim));
     }
-    approximation.dfdu.bottomRightCorner(stateConverter_.getJointDim(), stateConverter_.getJointDim()) = ocs2::matrix_t::Identity(stateConverter_.getJointDim(), stateConverter_.getJointDim());
-    approximation.f = computeStateDerivative<ocs2::scalar_t>(x, u, pinInterface_, stateConverter_);
+
     return approximation;
   }
 
